c946e3d304bc0bd8427b6306c8621691721282d5
ungit003 <ungit003@gmail.com>
2025-08-04 17:54:14 +0900

    [Bronze V] Title: A+B - 4, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,6 +1,5 @@
-while True:
-    try:
-        a, b = map(int, input().split())
-        print(a + b)
-    except EOFError:
-        break
+import sys
+
+for line in sys.stdin:
+    a, b = map(int, line.split())
+    print(a + b)
