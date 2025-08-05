75986bab35b4a1c6f8caeed6bd84a2240771f214
ungit003 <ungit003@gmail.com>
2025-08-05 18:23:04 +0900

    250805 알고리즘

--- 푸는 중/백준/250805_python.py
+++ 푸는 중/백준/250805_python.py
@@ -23,13 +23,13 @@ print(ans)"""
 
 # 5597
 
-students = list(range(1, 31))
+"""students = list(range(1, 31))
 subjects = list()
 for i in range(28):
     subjects.append(int(input()))
 for student in students:
     if student not in subjects:
-        print(student)
+        print(student)"""
 
 # 2738
 
