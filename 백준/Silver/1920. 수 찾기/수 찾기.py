19cd90b60a07bcdb921eed1f90dede0a66fe087c
ungit003 <ungit003@gmail.com>
2025-08-09 20:32:51 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 52272 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,23 +1,7 @@
-def finder(list, number, l_index, r_index):
-    if l_index > r_index:
-        return 0
-    center_index = (l_index + r_index) // 2
-    center_number = list[center_index]
-    if center_number == number:
-        return 1
-    elif center_number > number:
-        return finder(list, number, l_index, center_index - 1)
-    elif center_number < number:
-        return finder(list, number, center_index + 1, r_index)
-
 N = int(input())
-nums = list(map(int, input().split()))
+nums = set(map(int, input().split()))
 M = int(input())
 findings = list(map(int, input().split()))
 
-nums.sort()
-for finding in findings:
-    if nums[-1] < finding or nums[0] > finding:
-        print(0)
-        continue
-    print(finder(nums, finding, 0, N - 1))
+for target in findings:
+    print(1 if target in nums else 0)
