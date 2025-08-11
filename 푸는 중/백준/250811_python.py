9a0f724088e831f5e945649d199ee8b4ef790bcf
ungit003 <ungit003@gmail.com>
2025-08-11 18:24:29 +0900

    백준 10845

---
+++ 푸는 중/백준/250811_python.py
@@ -0,0 +1,68 @@
+# 10828
+
+"""N = int(input())
+orders = []
+for _ in range(N):
+    orders.append(input().split())
+# print(orders)
+
+stack = []
+for i in range(N):
+    if orders[i][0] == 'push':
+        stack.append(orders[i][1])
+    elif orders[i][0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            pop_number = stack.pop()
+            print(pop_number)
+    elif orders[i][0] == 'size':
+        print(len(stack))
+    elif orders[i][0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif orders[i][0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])"""
+
+# 10845
+
+"""from collections import deque
+
+N = int(input())
+orders = []
+for _ in range(N):
+    orders.append(input().split())
+
+queue = deque()
+for i in range(N):
+    if orders[i][0] == 'push':
+        queue.append(orders[i][1])
+    elif orders[i][0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            pop_left = queue.popleft()
+            print(pop_left)
+    elif orders[i][0] == 'size':
+        print(len(queue))
+    elif orders[i][0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif orders[i][0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif orders[i][0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])"""
+
