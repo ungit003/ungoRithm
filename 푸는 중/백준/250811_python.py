0133026a12abe24f9ca0442386f0bc720be1404f
ungit003 <ungit003@gmail.com>
2025-08-11 21:08:45 +0900

    백준 1018

--- 푸는 중/백준/250811_python.py
+++ 푸는 중/백준/250811_python.py
@@ -142,7 +142,8 @@ for case in PS:
     check_VPS(case)"""
 
 # 2164
-from collections import deque
+
+"""from collections import deque
 
 N = int(input())
 cards = deque(range(1, N+1))
@@ -154,4 +155,42 @@ while True:
     cards.popleft()
     moving = cards.popleft()
     cards.append(moving)
-print(cards[0])
+print(cards[0])"""
+
+# 1018
+
+def check_change(y, x):
+    case1, case2 = 0, 0
+    for i in range(y, y+8):
+        for j in range(x, x+8):
+            if i % 2 == 0:
+                if rec[i][j] != ans1[j-x]:
+                    case1 += 1
+                if rec[i][j] != ans2[j-x]:
+                    case2 += 1
+            else:
+                if rec[i][j] != ans2[j-x]:
+                    case1 += 1
+                if rec[i][j] != ans1[j-x]:
+                    case2 += 1
+    return min(case1, case2)
+
+M, N = map(int, input().split())
+rec = []
+for _ in range(M):
+    rec.append(input())
+
+ans1, ans2 = '', ''
+for i in range(8):
+    if i % 2 == 0:
+        ans1 += 'W'
+        ans2 += 'B'
+    else:
+        ans1 += 'B'
+        ans2 += 'W'
+
+ans_list = []
+for i in range(M-7):
+    for j in range(N-7):
+        ans_list.append(check_change(i, j))
+print(min(ans_list))
