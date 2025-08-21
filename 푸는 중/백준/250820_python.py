c958a5b65932c3afdffd4a8b3acfa96bd3805abb
ungit003 <ungit003@gmail.com>
2025-08-21 20:18:16 +0900

    250821 알고리즘

--- 푸는 중/백준/250820_python.py
+++ 푸는 중/백준/250820_python.py
@@ -1,6 +1,13 @@
 # 11021
 
-n = int(input())
-for i in range(n):
-    x, y = map(int, input().split())
-    print('Case #1:', x+y)
+# n = int(input())
+# for i in range(n):
+#     x, y = map(int, input().split())
+#     print('Case #1:', x+y)
+
+# import sys
+# n = int(sys.stdin.readline())
+
+# for i in range(n):
+#     x, y = map(int, sys.stdin.readline().split())
+#     print(f'Case #{i+1}: {x+y}')
---
+++ 푸는 중/백준/250821_python.py
@@ -0,0 +1,5 @@
+# 20492
+
+N = int(input())
+a, b = int(N*0.78), int(N - N*0.2*0.22)
+print(a, b)
