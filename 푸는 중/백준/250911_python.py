db89e35dc891a1be0ea3436ed7bb0c6cc3478040
ungit003 <ungit003@gmail.com>
2025-09-11 10:57:12 +0900

    250911 알고리즘

---
+++ 푸는 중/백준/250911_python.py
@@ -0,0 +1,17 @@
+# 30676
+
+l = int(input())
+if 620 <= l <= 780:
+    print('Red')
+elif 590 <= l < 620:
+    print('Orange')
+elif 570 <= l < 590:
+    print('Yellow')
+elif 495 <= l < 570:
+    print('Green')
+elif 450 <= l < 495:
+    print('Blue')
+elif 425 <= l < 450:
+    print('Indigo')
+elif 380 <= l < 425:
+    print('Violet')
