700c0303ba0b48438c7d4bcefb8dabea305cafe8
ungit003 <ungit003@gmail.com>
2025-09-17 23:57:57 +0900

    250917 알고리즘

---
+++ 푸는 중/250917_python.py
@@ -0,0 +1,11 @@
+# 27889
+
+dict = {
+    'NLCS': 'North London Collegiate School',
+    'BHA': 'Branksome Hall Asia',
+    'KIS': 'Korea International School',
+    'SJA': 'St. Johnsbury Academy'
+    }
+
+a = input()
+print(dict[a])
